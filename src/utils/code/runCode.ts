63edad2e5d4ec9b98065d15691799636902d7496
Joey Meere <100378695+joeymeere@users.noreply.github.com>
2024-04-11 22:05:46 -0400

    fix spl token parsing

--- src/utils/code/runCode.ts
+++ src/utils/code/runCode.ts
@@ -2,11 +2,14 @@ export async function runCode(code: string) {
     try {
         let args: [] = [];
         const pattern = /const web3 = require\("@solana\/web3\.js"\);\s*/;
-        const modifiedCode = code.replace(pattern, '');
+        const splPattern = /const spl = require\("@solana\/spl-token"\);\s*/;
+        const modify = code.replace(pattern, '');
+        const modifiedCode = modify.replace(splPattern, '');
 
         const web3 = await import("@solana/web3.js");
+        const spl = await import("@solana/spl-token");
 
-        let libs = { web3 };
+        let libs = { web3, spl };
 
         //const run = new Function("web3", 'return' + modifiedCode);
         let func = new Function(...Object.keys(libs), 'return ' + modifiedCode)(...Object.values(libs));
